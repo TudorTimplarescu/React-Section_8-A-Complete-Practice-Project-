6bd926c8d6618499b17eff43f11accf7b49df378
Tudor Timplarescu <tudor.timplarescu@gmail.com>
2023-03-13 13:44:36 +0200

    Finished

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -8,7 +8,7 @@ import styles from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop} />
+      <div className={styles.backdrop} onClick={props.onHandleError} />
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -17,7 +17,7 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={styles.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onHandleError}>Okay</Button>
         </footer>
       </Card>
     </div>
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,13 +9,22 @@ import styles from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState()
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredAge.trim().length === 0 || enteredUsername.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age'
+      })
       return; //If I return here,then the code below is not going to execute because return ends the function execution
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age'
+      })
       //enteredAge is a string and most times this expression works fine in JS,but with that + I assure that enteredAge is a number
       return;
     }
@@ -32,28 +41,32 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError() //Using setError(), will set it's value to undifined which is a falsy value so it will not render the ErrorModal component. It would also work with null as a value which is also a falsy value
+  }
+
   return (
     <div>
-    <ErrorModal title='An error occured!' message='Something went wrong!'/>
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
-        ></input>
-        <label htmlFor="age">Age</label>
-        <input
-          type="age"
-          id="age"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      {error && <ErrorModal title={error.title} message={error.message} onHandleError={errorHandler} />} {/*if error exists,render ErrorModal, otherwise if error would be undefined, nothing would be rendered here for this line. */}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          ></input>
+          <label htmlFor="age">Age</label>
+          <input
+            type="age"
+            id="age"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
     </div>
   );
 };
